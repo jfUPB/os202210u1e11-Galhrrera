2861be8458d3962adbf3a15cae4eaafcc99d6452
Gal <jegaleanoherrera@gmail.com>
2022-02-09 06:44:47 -0500

    Primeras pruebas realizadas

--- problem1/event.c
+++ problem1/event.c
@@ -1,12 +1,13 @@
 #include "event.h"
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 Event *CreateEvent(char *name)
 {
-    //Event *event = NULL;
     Event *event = malloc(sizeof(Event));
-
+    sscanf(name, "%s",event->eventName);
+    event->next=NULL;
     return event;
 }
 
--- problem1/eventList.c
+++ problem1/eventList.c
@@ -5,9 +5,7 @@
 
 EventList *CreateEventList(void)
 {
-    EventList *eventList = NULL;
-    //Se debe usar el malloc para crear la lista enlazada
-    //para el ejercicio se creará una lista enlazada de hasta 10 eventos
+    EventList *eventList = malloc(sizeof(eventList));
 
     return eventList;
 }
@@ -15,28 +13,77 @@ EventList *CreateEventList(void)
 void DestroyEventList(EventList *this)
 {
 
-
     free(this);
 }
 
 Event *SearchEvent(EventList *this, char *name)
 {
 
-
     return NULL;
 }
 
 void AddEvent(EventList *this, Event *event)
 {
-
+    if (this->isEmpty == 0)
+    {
+        this->head = event;
+        this->last = event;
+        this->isEmpty = 1;
+    }
+    else
+    {
+        this->last->next = event;
+        this->last = event;
+    }
 }
 
 void RemoveEvent(EventList *this, char *name)
 {
+    Event *a = this->head;
+    if (this->isEmpty == 0)
+    {
+        return;
+    }
+    // primero remover el último
+    else
+    {
+       
+        while (a != NULL)
+        {
+            if (*(this->head->eventName + 2) == *(name + 2))
+            {
+                this->head = this->head->next;
+                break;
+            }
+
+            else if (*(a->next->eventName + 2) == *(name + 2))
+            {
+                a->next = a->next->next;
+
+                break;
+            }
+
+            a = a->next;
+        }
+        if (this->head==NULL)
+        {
+            this->isEmpty = 0;
+        }
+    }
 }
 
 void ListEvents(EventList *this)
 {
+    Event *a = this->head;
     // imprime esto si la lista está vacía
-    printf("empty\n");
+    if (this->isEmpty == 0)
+        printf("empty\n");
+    else
+    {
+        while (a != NULL)
+        {
+            printf("%s\n", a->eventName); // BORRAR OJO
+            a = a->next;
+        }
+    }
 }
